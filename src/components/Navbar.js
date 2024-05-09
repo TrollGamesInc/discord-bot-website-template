ce82dde954de67ea61c2ffb8641003059d292999
Troll Games Inc <118114028+TrollGamesInc@users.noreply.github.com>
2024-05-09 08:10:05 -0400

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,7 @@ const Navbar = () => {
           <ul>
             <li>
               <Link className="h-color" href="/">
-                AstralAssisitant
+                AstralAssistant
               </Link>
             </li>
           </ul>
