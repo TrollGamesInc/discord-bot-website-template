fb781eea1fbb08152a10b2a192c2de7798c4787f
Troll Games Inc <118114028+TrollGamesInc@users.noreply.github.com>
2024-05-09 09:54:28 -0400

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -41,15 +41,15 @@ const About = () => {
 
         <section className="about-stats">
           <div className="about-stats-card">
-            <h4 className="h-color">17k+</h4>
+            <h4 className="h-color">10</h4>
             <p className="p-color">Total Servers</p>
           </div>
           <div className="about-stats-card">
-            <h4 className="h-color">94k+</h4>
+            <h4 className="h-color">29</h4>
             <p className="p-color">Total Users</p>
           </div>
           <div className="about-stats-card">
-            <h4 className="h-color">375k+</h4>
+            <h4 className="h-color">2.5k</h4>
             <p className="p-color">Total Commands Ran</p>
           </div>
         </section>
